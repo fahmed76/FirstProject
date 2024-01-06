be62aec0dd40ca68e0be575108d7f373bba71eab
fahmed76 <ahmedfarhana7671@gmail.com>
2024-01-06 12:11:55 -0500

    Initial commit.

---
+++ src/com/smart/tests/Test.java
@@ -0,0 +1,16 @@
+package com.smart.tests;
+
+public class Test {
+	
+	
+	@org.testng.annotations.Test
+	public void loginTest() {
+		
+		System.out.println("We are learning Git and GitHub through Eclipe.");
+		
+		
+		
+	}
+	
+
+}
